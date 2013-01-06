3da8e4f15a1f6681cffcadba84f55503b9d81ded
Nathan Zadoks <nathan@nathan7.eu>
2013-01-06 04:41:38 +0100

    revamped tests (phantomjs/mocha/chai)

--- test/test.js
+++ test/test.js
@@ -8,38 +8,21 @@ function _require(name, user) {
 }
 
 var microdata = _require('microdata', 'nathan7')
-  , _ = _require('underscore')
-  , assert = _require('assert')
 
-var result = microdata('http://data-vocabulary.org/Person')
-  , expected = [{ _type: "http://data-vocabulary.org/Person"
-                , photo: "http://diveintohtml5.info/examples/2000_05_mark.jpg"
-                , name: "Mark Pilgrim"
-                , title: "Developer advocate"
-                , affiliation: "Google, Inc."
-                , address: { _type: "http://data-vocabulary.org/Address"
-                           , "street-address": "100 Main Street"
-                           , locality: "Anytown"
-                           , region: "PA"
-                           , "postal-code": "19999"
-                           , "country-name": "USA"
-                           }
-                , url: ["http://diveintomark.org/", "http://www.google.com/profiles/pilgrim", "http://www.reddit.com/user/MarkPilgrim", "http://www.twitter.com/diveintomark"]
-                }]
-
-var success = false
-  , e
-
-try {
-  assert(_.isEqual(result, expected))
-  success = true
-}
-catch(e_) {
-  e = e_
-  success = false
-}
-
-var elem = document.createElement('h1')
-elem.textContent = 'tests ' + (success ? 'succeeded' : 'failed')
-elem.style.color = success ? 'green' : 'red'
-document.body.insertBefore(elem, document.body.firstChild)
+it('should find Mark Pilgrim', function() {
+  expect(microdata('http://data-vocabulary.org/Person')).to.deep.equal(
+    [{ _type: "http://data-vocabulary.org/Person"
+     , photo: "http://diveintohtml5.info/examples/2000_05_mark.jpg"
+     , name: "Mark Pilgrim"
+     , title: "Developer advocate"
+     , affiliation: "Google, Inc."
+     , address: { _type: "http://data-vocabulary.org/Address"
+                , "street-address": "100 Main Street"
+                , locality: "Anytown"
+                , region: "PA"
+                , "postal-code": "19999"
+                , "country-name": "USA"
+                }
+     , url: ["http://diveintomark.org/", "http://www.google.com/profiles/pilgrim", "http://www.reddit.com/user/MarkPilgrim", "http://www.twitter.com/diveintomark"]
+     }])
+});
