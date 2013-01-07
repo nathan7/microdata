e4035baff679387e81f1005d50080b82e8662efb
Nathan Zadoks <nathan@nathan7.eu>
2013-01-07 15:56:46 +0100

    test multiple complex values for a field

--- test/test.js
+++ test/test.js
@@ -16,13 +16,21 @@ it('should find Mark Pilgrim', function() {
      , name: "Mark Pilgrim"
      , title: "Developer advocate"
      , affiliation: "Google, Inc."
-     , address: { _type: "http://data-vocabulary.org/Address"
-                , "street-address": "100 Main Street"
-                , locality: "Anytown"
-                , region: "PA"
-                , "postal-code": "19999"
-                , "country-name": "USA"
-                }
+     , address: [{ _type: "http://data-vocabulary.org/Address"
+                 , "street-address": "P.O. Box 562"
+                 , locality: "Anytown"
+                 , region: "PA"
+                 , "postal-code": "12345"
+                 , "country-name": "USA"
+                 }
+                ,{ _type: "http://data-vocabulary.org/Address"
+                 , "street-address": "100 Main Street"
+                 , locality: "Anytown"
+                 , region: "PA"
+                 , "postal-code": "19999"
+                 , "country-name": "USA"
+                 }
+                ]
      , url: ["http://diveintomark.org/", "http://www.google.com/profiles/pilgrim", "http://www.reddit.com/user/MarkPilgrim", "http://www.twitter.com/diveintomark"]
      }])
 })
